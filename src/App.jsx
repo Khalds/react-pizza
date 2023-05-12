8f085e08d51eff56e5967143504f5d5e7dbe364f
khald <abv.khallid@mail.ru>
2023-05-12 17:36:24 +0300

    add back request

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
-
-import pizzas from './assets/pizzas.json';
-
+import { useEffect, useState } from 'react';
 import './scss/app.scss';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  //https://645e3f4912e0a87ac0eb6446.mockapi.io/items
+  useEffect(() => {
+    fetch('https://645e3f4912e0a87ac0eb6446.mockapi.io/items')
+      .then((res) => res.json())
+      .then((data) => {
+        setPizzas(data);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
