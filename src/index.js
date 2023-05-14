f429078f9be02f3a1f06e1c42c6b537bb7cf33fa
khald <abv.khallid@mail.ru>
2023-05-14 22:31:40 +0300

    add adaptive and cart styles for pizzas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import Cart from './pages/Cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
